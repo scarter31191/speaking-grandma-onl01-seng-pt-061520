cef3aec1fe5d5c82a419cfbc63c7aba5daa2c483
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-18 02:36:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   print "i love you grandma!"
   return "HUH?! SPEAK UP, SONNY!"
   
-if speak_to_grandma = "I LOVE YOU GRANDMA!"
+if speak_to_grandma == "I LOVE YOU GRANDMA!"
   print "NO, NOT SINCE 1938!"
   
   
